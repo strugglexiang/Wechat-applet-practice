0d23893dc21b6e0334ae7e10710cdd863bbfe5a3
strugglexiang <576430815@qq.com>
2019-04-14 17:50:15 +0800

    wxml

---
+++ pages/DataBind/DataBind.js
@@ -0,0 +1,10 @@
+Page({
+    data: {
+        message: 'my message',//内容
+        count: 0,//组件属性
+        check: true, //控制属性
+        list: [ 5, 4, 3],
+        name: 'strugglexiang',
+        age: 18,
+    }
+})
---
+++ pages/README.md
@@ -0,0 +1,5 @@
+HelloWorl HelloWorld
+DataBind 数据绑定
+ListRender 列表渲染
+IFRender 条件渲染
+Quote 引用
