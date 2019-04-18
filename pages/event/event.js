749d53200a899a7ad4646fdc8f9145fd8fe2aead
strugglexiang <576430815@qq.com>
2019-04-18 11:13:06 +0800

    事件

---
+++ pages/event/event.js
@@ -0,0 +1,23 @@
+Page({
+    tap(event) {
+        console.log('this is js tap event', event)
+    },
+    bindA(event) {
+        console.log('bindA', event)
+    },
+    bindB(event) {
+        console.log('bindB')
+    },
+    bindC(event) {
+        console.log('bindC')
+    },
+    capA(event) {
+        console.log('capA')
+    },
+    capB(event) {
+        console.log('capB')
+    },
+    capC(event) {
+        console.log('capC')
+    },
+})
